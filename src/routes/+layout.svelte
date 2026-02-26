ea6b87c23d4d40819573d24dfc7198c404981cd9
HoweZae <jdtomanan@up.edu.ph>
2026-02-26 18:08:44 +0800

    feat: add navbar, new logo, title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/pokeball.svg';
+	import NavBar from '$lib/components/NavBar.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Oak's Assistant</title>
+</svelte:head>
+
+<NavBar/>
+<div class="flex items-center bg-fixed">
+	<div class="container">
+		{@render children()}
+	</div>
+</div>
