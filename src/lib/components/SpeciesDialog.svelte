14e540f5a46da137411fa0dbb565e8da5e6b57c3
HoweZae <jdtomanan@up.edu.ph>
2026-02-27 10:49:29 +0800

    feat: init SpeciesDialog

---
+++ src/lib/components/SpeciesDialog.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { SpeciesDetails } from "@/types";
+  import { Content } from "./ui/dialog";
+
+  const details: SpeciesDetails = $props();
+</script>
+
+<Content>
+  <div></div>
+</Content>
