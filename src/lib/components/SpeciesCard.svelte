482aa5d9f12f1f1aaa20ddb3c451b1dac6b1b075
HoweZae <jdtomanan@up.edu.ph>
2026-02-27 10:49:19 +0800

    refactor: improve type handling; update SpeciesCard component to use Trigger for dialog

--- src/lib/components/SpeciesCard.svelte
+++ src/lib/components/SpeciesCard.svelte
@@ -1,40 +1,36 @@
 <script lang="ts">
-  const { name, id, url, type } = $props();
+  import { Trigger } from "./ui/dialog";
+
+  import type { SpeciesShort, SpeciesDetails } from "@/types";
+	import { getIdAsParam } from "@/utils";
+
+  const { name, id, url, types }: SpeciesShort = $props();
+
+  // svelte-ignore state_referenced_locally
+  const idParam: string = getIdAsParam(id);
   
   // svelte-ignore state_referenced_locally
-  const imageSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
-
-  import { onMount } from "svelte";
-
-  let isLoading = false;
-
-  onMount(async () => {
-    isLoading = true;
-    try {
-
-    } catch (e) {
-      alert("There was an error in fetching Pokemon details:\n"+e);
-    } finally {
-      isLoading = false;
-    }
-  });
+  const imageSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${idParam}.png`;
 </script>
 
 
-
-<a href={`species/${id}`}
-    class="card flex flex-col items-center
-            hover:ring hover:ring-accent">
+<Trigger class="card flex flex-col items-center hover:ring hover:ring-accent">
   <div class="flex flex-row justify-between items-center w-full">
-    <h4 class="font-semibold">{name}</h4>
-    <h5 class="font-mono">#{id}</h5>
+    <h4 class="font-semibold">
+      {name.charAt(0).toUpperCase() + name.slice(1)}
+    </h4>
+    <h5 class="font-mono">#{idParam}</h5>
   </div>
   <div class="flex flex-row justify-between items-end w-full">
-    <span class="bg-accent px-2 text-accent-foreground rounded">
-      {type.toUpperCase()}
-    </span>
+    <div class="flex flex-col space-y-1">
+      {#each types as type}
+        <span class="bg-accent px-1 text-accent-foreground rounded w-fit">
+          {type.toUpperCase()}
+        </span>
+      {/each}
+    </div>
     <img src={imageSrc}
           alt={name}
           class="size-24"/>
   </div>
-</a>
+</Trigger>
