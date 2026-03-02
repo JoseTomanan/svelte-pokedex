3435f5081267871569e9eeb2b6a74994714c9ee5
JoseTomanan <jdtomanan@up.edu.ph>
2026-03-03 01:23:49 +0800

    style: adjust layout padding and footer text color for consistency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,12 +11,12 @@
   <title>Oak's Assistant</title>
 </svelte:head>
 
-<NavBar/>
-<div class="flex justify-center bg-fixed min-h-[86vh]">
-  <div class="container">
+<div class="flex justify-center bg-fixed min-h-[92vh]">
+  <NavBar/>
+  <div class="pt-15 container">
     {@render children()}
   </div>
 </div>
 <footer class="flex justify-center py-4">
-  <p class="text-xs text-muted-foreground/65">Made with love, by Jose &hearts;</p>
+  <p class="text-xs text-muted-foreground/60">Made with love, by Jose &hearts;</p>
 </footer>
